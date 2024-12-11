ac298161bf3c94899294ba9a7ae43d3553cb3373
chae <39667376+YeomChaeeun@users.noreply.github.com>
2024-12-11 22:49:05 +0900

    Time: 222 ms (5.02%), Space: 59.3 MB (7.19%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -0,0 +1,20 @@
+function isPalindrome(s: string): boolean {
+    let word = s.toLowerCase();
+    console.log(word.length);
+    const reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\" ]/g;
+    
+    word = word.replace(reg, '');
+    console.log(word);
+    
+    if(word) {
+        for(let i = 0; i < word.length; i++) {
+            for(let j = word.length-i-1; j >= word.length-i-1; j--) {
+                console.log(word[i], '===', word[j])
+                if(word[i] !== word[j]) return false;
+            }   
+        }
+    } 
+    return true;
+    
+    
+};
