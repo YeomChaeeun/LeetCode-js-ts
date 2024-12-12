e4cbebc2f2b668788449810430d7e78006384a89
chae <39667376+YeomChaeeun@users.noreply.github.com>
2024-12-12 23:11:14 +0900

    Time: 15 ms (51.30%), Space: 55.7 MB (49.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
@@ -1,32 +1,20 @@
+/**
+ *
+ * @param nums
+ * @param k
+ */
 function topKFrequent(nums: number[], k: number): number[] {
     let obj = {}
-    
-    for(let i = 0; i < nums.length; i++) {
-        if(obj[nums[i]]) { 
-            ++obj[nums[i]]
-        } else {
-            obj[nums[i]] = 1
-        }
+
+    for(const num of nums) {
+        if(!obj[num])
+            obj[num] = 0
+        ++obj[num]
     }
-    // console.log(obj)
-    
-    let entries = Object.entries(obj)
-    // console.log(entries) // 배열로 변경
-    
-    let sort_arr = entries.sort((a, b) => Number(b[1]) - Number(a[1]));
+
+    // 배열로 변경 후 정렬
+    let sort_arr = Object.entries(obj).sort((a, b) => Number(b[1]) - Number(a[1]));
     // console.log(sort_arr)
-    
-    let result = [];
-    let l = 0;
-    for(const item of sort_arr) {
-        // console.log(item)
-        if(l == k) break;
-        result.push(Number(item[0]));
-        l++;
-    }
-    
-    // console.log(result)
-    
-    return result;
-    
+
+    return sort_arr.slice(0, k).map((i) => Number(i[0]));;
 };
