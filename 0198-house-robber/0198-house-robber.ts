af8380b306848b420d5873e85661ea52e77c9037
chae <39667376+YeomChaeeun@users.noreply.github.com>
2024-12-13 22:59:44 +0900

    Time: 14 ms (5.09%), Space: 53.3 MB (5.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.ts
+++ 0198-house-robber/0198-house-robber.ts
@@ -4,19 +4,33 @@ function rob(nums: number[]): number {
     
     // 0 + 2 ... 
     // 1 + 3 ...
- 
     // Max(인덱스 0부터 더한 것, 인덱스 1부터 더한 것)
     
+    // let max = 0
+    // let odd = 0
+    // let even = 0
+    // for(let i = 0; i < nums.length; i++) {
+    //     if(i % 2 == 0) {
+    //         console.log(nums[i], 'even >>>', even);
+    //         even += nums[i];
+    //     } else {
+    //         console.log(nums[i], 'odd >>>', odd);
+    //         odd += nums[i];
+    //     }
+    // }
+    // console.log(even, '===', odd);
+    // max = Math.max(even, odd);
+    
     let prev = 0
-    let curr = 0
+    let max = 0
     let temp = 0
     for(let i = 0; i < nums.length; i++) {
-        temp = curr; // 직전것을 바꿔줍니다 
-        curr = Math.max(prev + nums[i], curr);
-        prev = temp; 
+        temp = max
+        max = Math.max(prev + nums[i], max) // 이전의 값과 하나 이상 건너뛰고 더한 값 중 최대 값을 구함
+        prev = temp
         
-        console.log(temp, " - ", curr, " - ", prev)
+        console.log(temp, " - ", max, " - ", prev)
     };
-    console.log(curr);
-    return curr;
+    console.log(max);
+    return max;
 };
