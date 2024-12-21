a8c8cb0717a18b0d29a8af6f4aa6ddffd6164b79
chae <39667376+YeomChaeeun@users.noreply.github.com>
2024-12-21 12:03:46 +0900

    Time: 29 ms (97.15%), Space: 59.3 MB (96.31%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
@@ -1,43 +1,31 @@
+/**
+* 시간복잡도: O(nlogn) - 정렬로 인해
+* 공간복잡도: O(1) - 추가 공간 사용 없음
+*/
 function longestConsecutive(nums: number[]): number {
     
     if(nums.length < 2) 
         return nums.length; // 0, 1
     
     nums = nums.sort((a, b) => a - b)
-    // console.log(nums)
     
-//     let count = 1
-//     for(let i = 0; i < nums.length-1; i++) {
-//         if(nums[i] === nums[i+1]) {
-//             continue;
-//         } else if(nums[i] - nums[i+1] === 1) {
-//             count++;
-//         } else {
-//             break;
-//         }
-//     };
+    let currentLength = 1;    // 현재 연속 수열의 길이
+    let maxLength = 1;        // 전체 최대 연속 수열의 길이
     
-//     console.log(count);
-//     return count;
-    
-    let longest = 0;
-    let length = 1;
-    
-    for(let i = 0; i < nums.length-1; i++) {
-        if(nums[i] === nums[i + 1]) {
-            console.log(nums[i], '===', nums[i+1])
-            continue;
-        } else if(nums[i] + 1 === nums[i + 1] ) {
-            console.log(nums[i], '+ 1 =', nums[i+1])
-            length += 1;
+    for(let i = 1; i < nums.length; i++) {
+        if(nums[i] === nums[i - 1]) {
+            continue
+        } 
+        
+        if(nums[i] === nums[i - 1] + 1 ) {
+            currentLength++
         } else {
-            console.log(longest, ' - ', length)
-            longest = Math.max(length, longest);
-            length = 1;
+            // 연속이 끊긴 경우, maxLength 업데이트하고 currentLength 초기화
+            maxLength = Math.max(maxLength, currentLength);
+            currentLength = 1;
         }
     }
     
-    longest = Math.max(length, longest);
-    return longest;
-    
+    // 마지막 수열의 길이도 확인
+    return Math.max(maxLength, currentLength);
 }
