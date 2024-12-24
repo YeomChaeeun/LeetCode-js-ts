2c72a7a3b8fd3a5521aff38a0a97e325135951a8
chae <39667376+YeomChaeeun@users.noreply.github.com>
2024-12-24 17:18:45 +0900

    Time: 47 ms (35.18%), Space: 52.4 MB (58.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,12 +1,12 @@
 function twoSum(nums: number[], target: number): number[] {
-    
-    let output: number[] = [];
-    
+
+    let result: number[] = [];
+
     for(let i = 0; i < nums.length; i++) {
         for(let j = i+1; j<nums.length; j++) {
             if(nums[i]+nums[j] === target) {
-                output.push(i, j);
-                return output;
+                result.push(i, j);
+                return result;
             }
         }
     }
